73d4cecf785db3fbd0dab491332c2d8de43cdc13
Abdullayev984 <neymetk141@gmail.com>
2022-11-14 09:14:38 +0400

    first commit

---
+++ WcfService/IReverseService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IReverseService" in both code and config file together.
+    [ServiceContract]
+    public interface IReverseService
+    {
+        [OperationContract]
+        string ReverseWord(string word);
+    }
+}
---
+++ WcfService/ReverseService.svc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ReverseService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ReverseService.svc or ReverseService.svc.cs at the Solution Explorer and start debugging.
+    public class ReverseService : IReverseService
+    {
+        public string ReverseWord(string word)
+        {
+            char[] charArray = word.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
